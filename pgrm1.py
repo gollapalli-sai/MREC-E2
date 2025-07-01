024a312065a504f401fba24a78a91de28a580f25
gollapalli-sai <g.saiteja2110@gmail.com>
2025-07-01 10:10:48 +0530

    Update pgrm1.py
    secondmax

--- pgrm1.py
+++ pgrm1.py
@@ -1,7 +1,3 @@
-a=[1,2,6,7,3,4,5]
-max=a[0]
-for i in a:
-    if i > max:
-        max = i
-print("The maximum value in the list is:", max)        
-            
+a=[100,200,300,267,600]
+p=a.sort()
+print(a[-2])
